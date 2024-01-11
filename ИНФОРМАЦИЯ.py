b71ee8d0e03bea3df9602546b41a4c0c56ec1d06
Ilia <126543228+weoore@users.noreply.github.com>
2024-01-12 01:20:52 +0600

    ИНФОРМАЦИЯ.py

---
+++ ИНФОРМАЦИЯ.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Ргр по машинному обучению')
+
+st.markdown("---")
+
+st.header('Разработка Web-приложения (дашборда) для инференса (вывода) моделей ML и анализа данных')
+
+st.markdown("---")
+
+st.subheader('ФИО')
+st.markdown('Любаков Илья Александрович')
+
+st.subheader('Группа')
+st.markdown('МО-221')
+
+st.subheader('Фото')
+st.image('mi.jpg')
